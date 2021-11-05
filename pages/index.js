f6d2c6df18b1cb847b747d283fbe8a47bf18b810
Michael LaRoy <mike.laroy@gmail.com>
2021-11-04 17:04:34 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,56 @@
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { RippleAPI } from 'ripple-lib';
+
+const api = new RippleAPI({
+  server: 'wss://xrplcluster.com' // Public cluster
+});
+api.connect();
+
 export default function Home() {
+
+  let [isFetching, setIsFetching] = useState(false);
+  let [balances, setBalances] = useState([]);
+  let [message, setMessage] = useState('');
+  let [address, setAddress] = useState('');
+
+  const getBalance = async (address) => {
+    setIsFetching(true);
+    try {
+      const balances = api.getBalances(address)
+      return balances;
+    } catch(err) {
+      setMessage( err.message );
+      return err;
+    } finally {
+      setIsFetching(false);
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({e});
+
+    const balances = await getBalance(address);
+    console.log({ balances });
+    setBalances(balances);
+  }
+
+  // useEffect( () => {
+  //   const newApi = new RippleAPI({
+  //     server: 'wss://xrplcluster.com' // Public cluster
+  //   });
+
+  //   setApi( newApi );
+
+  //   api.connect();
+
+  //   console.log(api);
+  // }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,55 +61,44 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Check my XRP Balance
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <form className={styles.description} onSubmit={handleSubmit}>
+          <label htmlFor="address">
+            <span className={styles.formLabel}>Address</span>
+            <input id="address" type="text" placeholder="123abc..." value={address} onChange={(e) => setAddress(e.target.value) } className={styles.formInput}/>
+          </label>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <button type="submit" className={styles.formSubmit}>Submit</button>
+        </form>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        {message && (
+          <p className={styles.description}>
+            {message}
+          </p>
+        )}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        {isFetching && (
+          <p>Loading...</p>
+        )}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {balances && (
+          <p className={styles.description}>
+            {balances.map( balance => {
+              return (
+                <li key={balance.value}>{balance.currency} Balance: {balance.value}</li>
+              )
+            })}
+          </p>
+        )}
+
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          href="https://mikelaroy.ca/">
+          Made By: Michael LaRoy
         </a>
       </footer>
     </div>
